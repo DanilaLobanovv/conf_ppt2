628cdfa3b0063c9e58c02c1eb2e5a65dd467fe73
DanilaLobanovv <lobanov.dani1a@ya.ru>
2025-10-26 15:09:29 +0300

    second

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
+import json
 import yaml
+import urllib.request
 from urllib.parse import urlparse
+
 
 try:
     with open(sys.argv[1], 'r') as file:
@@ -29,7 +32,7 @@ if not isinstance(data["mode"], int):
     print("mode is incorrect")
     exit()
 
-if not isinstance(data["package-version"], int):
+if not isinstance(data["package-version"], str):
     print("package-version is incorrect")
     exit()
 
@@ -45,4 +48,14 @@ if not isinstance(data["package-filter-str"], str):
     print("package-filter-str is incorrect")
     exit()
 
-print(data)
+url = data["URL"]
+package_name = data["package-name"]
+version = data["package-version"]
+full_url = f"{url}/{package_name}/{version}"
+
+with urllib.request.urlopen(full_url) as response:
+    data2 = json.loads(response.read().decode())
+    dependencies = data2.get('dependencies', {})
+
+for dep_name, dep_version in dependencies.items():
+        print(f"{dep_name}: {dep_version}")
